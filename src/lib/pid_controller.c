bc88ff3c1d0c342b874af6d9c24735a46adb78d8
Gustavo Francisco <guga_581@hotmail.com>
2020-09-07 14:12:57 -0300

    fix pid controller

--- src/lib/pid_controller.c
+++ src/lib/pid_controller.c
@@ -19,7 +19,7 @@
  * @author Lucas Augusto Tansini, Lucas Valandro da Rocha, Gustavo Francisco
  * @date Sept 2020
  * @brief Source file for PID Controller utilized in quanser-2dsfje project for ENG10032
- */
+ **/
 
 
 // Project Headers
@@ -28,11 +28,11 @@
 // C Headers
 #include <stdio.h>
 
-/*
+/**
 * @brief Structure holding PID Controller data
 *
 * This structure is used to manage all PID Controller data. Gain variables are set here, for example.
-*/
+**/
 struct SPidController
 {
   double Kp; /**Proportional Gain in PID Controller. */
@@ -40,18 +40,20 @@ struct SPidController
   double Kd; /**Derivative Gain in PID Controller. */
   double joint_desired_position; /**Variable that holds the desired position for the joint to go to. Is informed in the terminal by the user.*/
   double integral_sum_of_errors; /** Variable that holds all erro sum for integral part of PID calculation.*/
+  double previous_error; /** Previous error, for derivative calculation. */
 } pidController;
 
-/*
+/**
 * @brief Variable for setting gains in PID Controller
 *
 * This function sets a gain value for the PID Controller
 * @param gain_type Enumerator indicating which Gain the user wants to set.
 * @param new_gain_value Value to set the selected gain.
 * @return void
-*/
+**/
 void set_gain_value(int gain_type, double new_gain_value)
 {
+  pidController.previous_error = 0.0;
   switch(gain_type)
   {
     case KP_GAIN:
@@ -76,26 +78,26 @@ void set_gain_value(int gain_type, double new_gain_value)
   }
 }
 
-/*
+/**
 * @brief Variable for sets PID target value
 *
 * This function sets a gain value for the PID Controller
 * @param desired_value Target value position to PID controller.
 * @return void
-*/
+**/
 void set_desired_position(float desired_value)
 {
   pidController.joint_desired_position = desired_value;
 }
 
-/*
+/**
 * @brief Runs PID Controller
 *
 * This function runs PID controller in order to reach the target value position set in PID data structure.
 * @param joint_current_position Value of the current join position in radians
 * @param pid_voltage_value Value of the PID Voltage value to be applie on motor
 * @return float Error between desired position and current position
-*/
+**/
 float run_pid_controller(float joint_current_position, float* pid_voltage_value)
 {
     float error = pidController.joint_desired_position - joint_current_position;
@@ -103,10 +105,11 @@ float run_pid_controller(float joint_current_position, float* pid_voltage_value)
     float derivatide_part = 0.0;
     float integral_part = 0.0;
 
+	
     pidController.integral_sum_of_errors += error;
 
     proportional_part = pidController.Kp * error;
-    derivatide_part = pidController.Kd * error;
+    derivatide_part = pidController.Kd * (error - pidController.previous_error);
     integral_part = pidController.Ki * pidController.integral_sum_of_errors;
 
     // Calculates the Voltage according to errors and PID parts
@@ -121,7 +124,9 @@ float run_pid_controller(float joint_current_position, float* pid_voltage_value)
       pid_controller_target_voltage = MOTOR_MAX_VOLTAGE_PID;
     }
 
-    pid_voltage_value = &pid_controller_target_voltage;
+    *pid_voltage_value = pid_controller_target_voltage;
+    
+    pidController.previous_error = error;
     // Return PID error
     return error;
 }
--- src/test/pid_unit_test/pid_unit_test.c
+++ src/test/pid_unit_test/pid_unit_test.c
@@ -24,33 +24,40 @@
 #include <unistd.h>
 #include <float.h>
 #include <time.h>
+#include <math.h>
 
 int main(int argc,char * argv[])
 {
-  set_gain_value(KP_GAIN, 10);
-  set_gain_value(KI_GAIN, 0);
-  set_gain_value(KD_GAIN, 0);
+  set_gain_value(KP_GAIN, 0.9);
+  set_gain_value(KI_GAIN, 0.05);
+  set_gain_value(KD_GAIN, 0.01);
 
   float pid_error = FLT_MAX;
 
   // Starting with 10
-  float current_position_test = 10;
+  float current_position_test = 0;
 
   // Setting desired position to 100
-  set_desired_position(100);
+  set_desired_position(3.141519);
 
   // Not used
-  float* pid_voltage_return_dummy = NULL;
+  float pid_voltage_return_dummy;
+  
+  int interations = 0;
+  
+  printf("Initial position %f, desired position 3.141519 \n", current_position_test);
 
-  while(pid_error > ACCEPTABLE_PID_ERROR)
+  while(fabs(pid_error) > ACCEPTABLE_PID_ERROR)
   {
-    pid_error = run_pid_controller(current_position_test, pid_voltage_return_dummy);
-    printf("PID Error after one PID run: %f.\n", pid_error);
+	interations++;
+    pid_error = run_pid_controller(current_position_test, &pid_voltage_return_dummy);
+    printf("PID Error after one PID run: %f.\n", fabs(pid_error));
+    float teste = pid_voltage_return_dummy;
 
     // Simulating position advance
-    current_position_test += 0.5;
+    current_position_test += teste;
   }
-
-  printf("PID Successfully ended executing with error < 2%.\n", pid_error);
+  printf("Final position: %f \n", current_position_test);
+  printf("PID Successfully ended executing with error < 2%, attempts %d.\n",interations);
   return 0;
 }
